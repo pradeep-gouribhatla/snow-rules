63506954139a7cf2e2e9c7c28f939fc36472d501
pradeep.gouribhatla <pradeep.gouribhatla@gmail.com>
2017-11-02 23:14:17 +0530

    minor changes

--- _SNOW0017.js
+++ _SNOW0017.js
@@ -3,85 +3,72 @@
 //------------------------------------------------------------------------------
 
 module.exports = function(context) {
+    var GLIDERECORD = "GlideRecord";
+    var SETVALUE = "setValue";
+    var AUTOSYSFIELDS = "autoSysFields";
+    var _activeCallee = [];
 
-  var GLIDERECORD = "GlideRecord";
-  var SETVALUE ="setValue";
-  var AUTOSYSFIELDS = "autoSysFields";
-  var _activeCallee = [];
+    var _checkStringValidity = function(node) {
+        if (node.type == "Literal") {
+            return node.value;
+        } else {
+            return true;
+        }
+    };
 
-  var _checkStringValidity = function(node){
-    console.log(node);
-    if(node.type == "Literal"){
-      return node.value;
-    }else{
-      return true;
-    }
-  }
+    var _getArgumentsList = function(node) {
+        return node.parent ? (node.parent.arguments ? node.parent.arguments : null) : null;
+    };
 
-  var _getArgumentsList = function(node){
-    return node.parent ?
-            			(node.parent.arguments ? node.parent.arguments:null)
-              					:null;
-  }
+    var _getGlideVariableOrAssignee = function(activeNode) {
+        var name;
+        switch (activeNode.type) {
+            case "VariableDeclarator":
+                name = activeNode.id.name;
+                _activeCallee.push(name);
 
-  var _getGlideVariableOrAssignee = function(activeNode){
-    var name;
-    switch(activeNode.type){
-        case "VariableDeclarator":
-        		name = activeNode.id.name;
-        		_activeCallee.push(name);
+                break;
+            case "AssignmentExpression":
+                name = activeNode.left.name;
+                _activeCallee.push(name);
 
-        		break;
-      	case "AssignmentExpression":
-        		name = activeNode.left.name;
-        		_activeCallee.push(name);
+                break;
+        }
+    };
 
-        		break;
-    }
-
-  }
-
-  var _checkForSetValue = function(nodePropertyName,node){
-
-    if(nodePropertyName && nodePropertyName === SETVALUE ){
-              var argumentsList = node.parent ?
-                  				(node.parent.arguments ? node.parent.arguments:null)
-              					:null;
-              if(argumentsList && argumentsList.length!==2){
-                context.report(node,"setValue must have 2 parameters");
+    var _checkForSetValue = function(nodePropertyName, node) {
+        if (nodePropertyName && nodePropertyName === SETVALUE) {
+            var argumentsList = node.parent ? (node.parent.arguments ? node.parent.arguments : null) : null;
+            if (argumentsList && argumentsList.length !== 2) {
+                context.report(node, "setValue must have 2 parameters");
                 return;
-              }else if(argumentsList &&
-                       argumentsList.length === 2 &&
-                       (!_checkStringValidity(argumentsList[0]) ||
-                       !_checkStringValidity(argumentsList[1]))
-                      ){
-              	context.report(node,"setValue parameter shoudnot contain empty string");
-              }
-
+            } else if (
+                argumentsList &&
+                argumentsList.length === 2 &&
+                (!_checkStringValidity(argumentsList[0]) || !_checkStringValidity(argumentsList[1]))
+            ) {
+                context.report(node, "setValue parameter shoudnot contain empty string");
             }
+        }
+    };
 
-  }
+    var getTypeOfParent = function(parent) {
+        return parent.type;
+    };
 
-  var getTypeOfParent = function(parent){
-    return parent.type;
-  }
+    return {
+        condition(record) {
+            return record.sys_class_name == "sys_script_include" || record.sys_class_name == "sys_script";
+        },
 
-
-
-  return {
-    "condition" (record) {
-      return record.sys_class_name == 'sys_script_include' 
-              || record.sys_class_name == 'sys_script';
-		},
-
-    "NewExpression" : function(node){
-      if(node.callee && node.callee.name && node.callee.name === GLIDERECORD){
-      	_getGlideVariableOrAssignee(node.parent);
-      }
-    },
-    "MemberExpression" : function(node){
-      	var memberExpName = node.object.name || null;
-        /*
+        NewExpression: function(node) {
+            if (node.callee && node.callee.name && node.callee.name === GLIDERECORD) {
+                _getGlideVariableOrAssignee(node.parent);
+            }
+        },
+        MemberExpression: function(node) {
+            var memberExpName = node.object.name || null;
+            /*
         	Best case scenario if var gr = new GlideRecord('sv') and setValue
             and autoSysFields are called in it. So below if will validate and
             error is displayed.
@@ -90,11 +77,10 @@ module.exports = function(context) {
             else will evaluate and look for setValue and autoSysFields irrespective
             of value in _activeCallee[].
         */
-      	var nodePropertyName = node.property ? node.property.name : null;
-      	if(memberExpName && _activeCallee.indexOf(memberExpName)>-1){
-        	_checkForSetValue(nodePropertyName,node);
+            var nodePropertyName = node.property ? node.property.name : null;
+            if (memberExpName && _activeCallee.indexOf(memberExpName) > -1) {
+                _checkForSetValue(nodePropertyName, node);
+            }
         }
-    }
-  };
+    };
 };
-
--- _SNOW0018.js
+++ _SNOW0018.js
@@ -3,69 +3,60 @@
 //------------------------------------------------------------------------------
 
 module.exports = function(context) {
+    var GLIDERECORD = "GlideRecord";
+    var SETVALUE = "setValue";
+    var AUTOSYSFIELDS = "autoSysFields";
+    var _activeCallee = [];
 
-  var GLIDERECORD = "GlideRecord";
-  var SETVALUE ="setValue";
-  var AUTOSYSFIELDS = "autoSysFields";
-  var _activeCallee = [];
+    var _checkStringValidity = function(string) {
+        if (string) return true;
+        return false;
+    };
 
-  var _checkStringValidity = function(string){
-  	if(string)
-      return true;
-    return false;
-  }
+    var _getArgumentsList = function(node) {
+        return node.parent ? (node.parent.arguments ? node.parent.arguments : null) : null;
+    };
 
-  var _getArgumentsList = function(node){
-    return node.parent ?
-            			(node.parent.arguments ? node.parent.arguments:null)
-              					:null;
-  }
+    var _getGlideVariableOrAssignee = function(activeNode) {
+        var name;
+        switch (activeNode.type) {
+            case "VariableDeclarator":
+                name = activeNode.id.name;
+                _activeCallee.push(name);
 
-  var _getGlideVariableOrAssignee = function(activeNode){
-    var name;
-    switch(activeNode.type){
-        case "VariableDeclarator":
-        		name = activeNode.id.name;
-        		_activeCallee.push(name);
+                break;
+            case "AssignmentExpression":
+                name = activeNode.left.name;
+                _activeCallee.push(name);
 
-        		break;
-      	case "AssignmentExpression":
-        		name = activeNode.left.name;
-        		_activeCallee.push(name);
-
-        		break;
-    }
-
-  }
-
-  var _checkForAutoSysFields = function(nodePropertyName,node){
-    if(nodePropertyName && nodePropertyName === AUTOSYSFIELDS){
-        var argumentsList = _getArgumentsList(node);
-      	if(argumentsList && argumentsList.length!==1){
-        	context.report(node,"autoSysFields must have boolean Parameter")
-        }else if(argumentsList && argumentsList.length===1 && argumentsList[0].value===false){
-        	context.report(node,"Avoid using autoSysFields with parameter as false");
+                break;
         }
-        console.log(argumentsList)
+    };
 
-    }
-  }
+    var _checkForAutoSysFields = function(nodePropertyName, node) {
+        if (nodePropertyName && nodePropertyName === AUTOSYSFIELDS) {
+            var argumentsList = _getArgumentsList(node);
+            if (argumentsList && argumentsList.length !== 1) {
+                context.report(node, "autoSysFields must have boolean Parameter");
+            } else if (argumentsList && argumentsList.length === 1 && argumentsList[0].value === false) {
+                context.report(node, "Avoid using autoSysFields with parameter as false");
+            }
+        }
+    };
 
-  var getTypeOfParent = function(parent){
-    return parent.type;
-  }
+    var getTypeOfParent = function(parent) {
+        return parent.type;
+    };
 
-
-
-  return {
-    "NewExpression" : function(node){
-      if(node.callee && node.callee.name && node.callee.name === GLIDERECORD){
-      	_getGlideVariableOrAssignee(node.parent);
-      }
-    },
-    "MemberExpression" : function(node){
-      	var memberExpName = node.object.name || null;
-        /*
+    return {
+        NewExpression: function(node) {
+            if (node.callee && node.callee.name && node.callee.name === GLIDERECORD) {
+                _getGlideVariableOrAssignee(node.parent);
+            }
+        },
+        MemberExpression: function(node) {
+            var memberExpName = node.object.name || null;
+            /*
         	Best case scenario if var gr = new GlideRecord('sv') and setValue
             and autoSysFields are called in it. So below if will validate and
             error is displayed.
@@ -74,12 +65,10 @@ module.exports = function(context) {
             else will evaluate and look for setValue and autoSysFields irrespective
             of value in _activeCallee[].
         */
-      	var nodePropertyName = node.property ? node.property.name : null;
-      	if(memberExpName && _activeCallee.indexOf(memberExpName)>-1){
-          	_checkForAutoSysFields(nodePropertyName,node);
+            var nodePropertyName = node.property ? node.property.name : null;
+            if (memberExpName && _activeCallee.indexOf(memberExpName) > -1) {
+                _checkForAutoSysFields(nodePropertyName, node);
+            }
         }
-    }
-  };
+    };
 };
-
-
