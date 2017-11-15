b1b98f0f3c614b3129341a45f08856ff7b989e62
Pradeep Gouribhatla <pradeep.gouribhatla@gmail.com>
2017-11-15 12:32:06 +0530

    Update _SNOW0014.js

--- _SNOW0014.js
+++ _SNOW0014.js
@@ -2,13 +2,20 @@
 // Rule Definition
 //------------------------------------------------------------------------------
 
+function isOOTBMethod(node) {
+	return node && node.parent && node.parent.type == "FunctionExpression" && node.parent.parent && node.parent.parent.type == "Property" && node.parent.parent.key && node.parent.parent.key.type == "Identifier" && node.parent.parent.key.name == "initialize";
+}
+
 module.exports = function(context){
 	return {
 		
 		"BlockStatement:exit"(node) {
 			if(node && node.body && node.body.length  === 0) {
-				context.report(node, "Scripts should not have an empty blocks defined ")
-			}
+                        console.log('aaaa -> ' + node.parent.parent.key.type)
+                if(!isOOTBMethod(node)) {
+					context.report(node, "Scripts should not have an empty blocks defined ");
+                }
+			}          
 		}
 	}
 	
